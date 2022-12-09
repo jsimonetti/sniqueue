f78451deb8fb6707e6c4519389404716f9326647
Jeroen Simonetti <jeroen@simonetti.nl>
2022-12-09 09:15:55 +0100

    [chore] add go1.19 support and remove 1.17
    Signed-off-by: Jeroen Simonetti <jeroen@simonetti.nl>

--- internal/qtls/go119.go
+++ internal/qtls/go119.go
@@ -3,4 +3,15 @@
 
 package qtls
 
-var _ int = "quic-go doesn't build on Go 1.19 yet."
+import (
+	"crypto/cipher"
+
+	"github.com/marten-seemann/qtls-go1-19"
+)
+
+type CipherSuiteTLS13 = qtls.CipherSuiteTLS13
+
+// AEADAESGCMTLS13 creates a new AES-GCM AEAD for TLS 1.3
+func AEADAESGCMTLS13(key, fixedNonce []byte) cipher.AEAD {
+	return qtls.AEADAESGCMTLS13(key, fixedNonce)
+}
