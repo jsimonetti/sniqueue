2269e4f49568e501acb55056ff42f008e06d52be
Jeroen Simonetti <jeroen@simonetti.nl>
2023-10-11 11:34:52 +0200

    [flake] update vendorSha
    Signed-off-by: Jeroen Simonetti <jeroen@simonetti.nl>

--- default.nix
+++ default.nix
@@ -9,7 +9,7 @@ buildGoModule rec {
   name = "sniqueue";
   inherit src;
 
-  vendorSha256 = "sha256-MCyUTa46iMp3HPSLTz7SpSxMkvd2U+g80ODSm3lQXqg=";
+  vendorSha256 = "sha256-i6iublVty6YUL6emNKV6ww3Kx2C3x+6VJ8cd3gfUtuA=";
 
   subPackages = [ "cmd/sniqueue" ];
 
