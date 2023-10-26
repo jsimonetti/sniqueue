613c13682cc9c581d3d2f84f34bd68ff494eec48
Jeroen Simonetti <jsimonetti@users.noreply.github.com>
2023-10-26 11:11:27 +0200

    [qtls] move initial decryption locally (#47)
    * [qtls] move initial decryption locally
    
    This will remove the dependency of upstream qtls.
    
    Signed-off-by: Jeroen Simonetti <jeroen@simonetti.nl>
    
    * [qtls] remove go1.19 qtls
    
    This uses the internal qtls initial decrypter
    
    Signed-off-by: Jeroen Simonetti <jeroen@simonetti.nl>
    
    * [go] update gomod
    
    Signed-off-by: Jeroen Simonetti <jeroen@simonetti.nl>
    
    * [chore] add 1.21 to actions
    
    Signed-off-by: Jeroen Simonetti <jeroen@simonetti.nl>
    
    * [flake] update vendorsha
    
    Signed-off-by: Jeroen Simonetti <jeroen@simonetti.nl>
    
    * [chore] move actions
    
    Signed-off-by: Jeroen Simonetti <jeroen@simonetti.nl>
    
    ---------
    
    Signed-off-by: Jeroen Simonetti <jeroen@simonetti.nl>

--- go.mod
+++ go.mod
@@ -6,19 +6,16 @@ require (
 	github.com/Lochnair/go-patricia v2.3.3+incompatible
 	github.com/florianl/go-nfqueue v1.3.1
 	github.com/google/go-cmp v0.6.0
-	github.com/quic-go/qtls-go1-19 v0.3.3
-	github.com/quic-go/qtls-go1-20 v0.2.3
 	github.com/shomali11/util v0.0.0-20200329021417-91c54758c87b
 	golang.org/x/crypto v0.14.0
 )
 
 require (
-	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/josharian/native v1.0.0 // indirect
 	github.com/mdlayher/netlink v1.6.0 // indirect
 	github.com/mdlayher/socket v0.1.1 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
-	golang.org/x/net v0.17.0 // indirect
+	golang.org/x/net v0.10.0 // indirect
 	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
 	golang.org/x/sys v0.13.0 // indirect
 )
---
+++ internal/qtls/qtls.go
@@ -0,0 +1,84 @@
+package qtls
+
+import (
+	"crypto"
+	"crypto/aes"
+	"crypto/cipher"
+)
+
+type CipherSuiteTLS13 struct {
+	ID     uint16
+	KeyLen int
+	Hash   crypto.Hash
+	AEAD   func(key, fixedNonce []byte) aead
+}
+
+func (c *CipherSuiteTLS13) IVLen() int {
+	return aeadNonceLength
+}
+
+type aead interface {
+	cipher.AEAD
+
+	// explicitNonceLen returns the number of bytes of explicit nonce
+	// included in each record. This is eight for older AEADs and
+	// zero for modern ones.
+	explicitNonceLen() int
+}
+
+func AEADAESGCMTLS13(key, nonceMask []byte) aead {
+	if len(nonceMask) != aeadNonceLength {
+		panic("tls: internal error: wrong nonce length")
+	}
+	aes, err := aes.NewCipher(key)
+	if err != nil {
+		panic(err)
+	}
+	aead, err := cipher.NewGCM(aes)
+	if err != nil {
+		panic(err)
+	}
+
+	ret := &xorNonceAEAD{aead: aead}
+	copy(ret.nonceMask[:], nonceMask)
+	return ret
+}
+
+const (
+	aeadNonceLength = 12
+)
+
+// xorNonceAEAD wraps an AEAD by XORing in a fixed pattern to the nonce
+// before each call.
+type xorNonceAEAD struct {
+	nonceMask [aeadNonceLength]byte
+	aead      cipher.AEAD
+}
+
+func (f *xorNonceAEAD) NonceSize() int        { return 8 } // 64-bit sequence number
+func (f *xorNonceAEAD) Overhead() int         { return f.aead.Overhead() }
+func (f *xorNonceAEAD) explicitNonceLen() int { return 0 }
+
+func (f *xorNonceAEAD) Seal(out, nonce, plaintext, additionalData []byte) []byte {
+	for i, b := range nonce {
+		f.nonceMask[4+i] ^= b
+	}
+	result := f.aead.Seal(out, f.nonceMask[:], plaintext, additionalData)
+	for i, b := range nonce {
+		f.nonceMask[4+i] ^= b
+	}
+
+	return result
+}
+
+func (f *xorNonceAEAD) Open(out, nonce, ciphertext, additionalData []byte) ([]byte, error) {
+	for i, b := range nonce {
+		f.nonceMask[4+i] ^= b
+	}
+	result, err := f.aead.Open(out, f.nonceMask[:], ciphertext, additionalData)
+	for i, b := range nonce {
+		f.nonceMask[4+i] ^= b
+	}
+
+	return result, err
+}
