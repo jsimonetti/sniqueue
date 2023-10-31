46c5a3bccf8ea3e5d843f77e1ca095965e5318f6
Jeroen Simonetti <jeroen@simonetti.nl>
2023-10-31 14:27:49 +0100

    [nix] test new modules action
    Signed-off-by: Jeroen Simonetti <jeroen@simonetti.nl>

---
+++ build.nix
@@ -0,0 +1,21 @@
+{ pkgs ? import <nixpkgs> { }
+, src ? ./.
+, buildGoModule
+, fetchFromGithub
+, lib
+,
+}:
+
+buildGoModule rec {
+  name = "sniqueue";
+  inherit src;
+
+  vendorSha256 = lib.fakeSha256
+
+  subPackages = [ "cmd/sniqueue" ];
+
+  meta = {
+    description = "SNIQueue";
+    homepage = "https://github.com/jsimonetti/sniqueue";
+  };
+}
